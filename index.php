361f784b5ee27e5073749cfff52b5961d9054f29
jonakrampe <jona.krampe@newcubator.com>
2024-12-11 08:52:27 +0100

    added tailwind

--- index.php
+++ index.php
@@ -5,25 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mensa-Plan Seite</title>
     <style>
-        body {
-            font-family: Arial, sans-serif;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh;
-            margin: 0;
-            background-color: #f0f0f0;
-        }
-        .container {
-            text-align: center;
-            padding: 20px;
-            border-radius: 8px;
-            background-color: #ffffff;
-            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
-        }
-        h1 {
-            color: #333;
-        }
+        <?php include './src/input.css'; ?>
     </style>
 </head>
 <body>
